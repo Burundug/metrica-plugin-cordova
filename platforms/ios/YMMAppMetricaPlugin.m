43c5d2659b36a55497962e9ec639c7c03ba52d17
burundug <iburundugi@gmail.com>
2025-07-15 10:15:03 -0300

    update IOS

--- platforms/ios/YMMAppMetricaPlugin.m
+++ platforms/ios/YMMAppMetricaPlugin.m
@@ -1,6 +1,5 @@
 /*
  * Version for Cordova/PhoneGap
- * © 2017-2019 YANDEX
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * https://yandex.com/legal/appmetrica_sdk_agreement/
@@ -8,12 +7,15 @@
 
 #import "YMMAppMetricaPlugin.h"
 
-#import <YandexMobileMetrica/YandexMobileMetrica.h>
+#import <AppMetricaCore/AppMetricaCore.h>
+#import <AppMetricaCrashes/AppMetricaCrashes.h>
 #import <CoreLocation/CoreLocation.h>
 
 static bool gYMMIsAppMetricaActivated = false;
 
 @implementation YMMAppMetricaPlugin
+
+
 
 - (void)handleOpenURL:(NSNotification *)notification
 {
@@ -22,7 +24,7 @@ static bool gYMMIsAppMetricaActivated = false;
     NSURL *URL = [notification object];
     if ([URL isKindOfClass:[NSURL class]]) {
         [self dispatchAsync:^{
-            [YMMYandexMetrica handleOpenURL:URL];
+            [AMAAppMetrica trackOpeningURL:URL];
         }];
     }
 }
@@ -35,6 +37,7 @@ static bool gYMMIsAppMetricaActivated = false;
 
     [self dispatchAsync:^{
         [[self class] activateWithConfigurationDictionary:configurationDictionary];
+
     }];
 }
 
@@ -44,7 +47,7 @@ static bool gYMMIsAppMetricaActivated = false;
     NSDictionary *eventParams = [command argumentAtIndex:1 withDefault:nil andClass:[NSDictionary class]];
 
     [self dispatchAsync:^{
-        [YMMYandexMetrica reportEvent:eventName
+        [AMAAppMetrica reportEvent:eventName
                            parameters:eventParams
                             onFailure:[self failureCallbackForCommand:command]];
     }];
@@ -54,12 +57,14 @@ static bool gYMMIsAppMetricaActivated = false;
 {
     NSString *errorName = [command argumentAtIndex:0 withDefault:nil andClass:[NSString class]];
     NSString *errorReason = [command argumentAtIndex:1 withDefault:nil andClass:[NSString class]];
-
+    AMAError *underlyingError = [AMAError errorWithIdentifier:@"Underlying AMAError"];
     [self dispatchAsync:^{
-        NSException *exception = [NSException exceptionWithName:errorName reason:errorReason userInfo:nil];
-        [YMMYandexMetrica reportError:errorName
-                            exception:exception
-                            onFailure:[self failureCallbackForCommand:command]];
+        AMAError *error = [AMAError errorWithIdentifier:errorName
+                                                message:errorReason
+                                             parameters:@{ @"foo": @"bar" }
+                                              backtrace:NSThread.callStackReturnAddresses
+                                        underlyingError:underlyingError];
+        [[AMAAppMetricaCrashes crashes] reportError:error  onFailure:[self failureCallbackForCommand:command]];
     }];
 }
 
@@ -69,7 +74,7 @@ static bool gYMMIsAppMetricaActivated = false;
 
     [self dispatchAsync:^{
         CLLocation *location = [[self class] locationForDictionary:locationDictionary];
-        [YMMYandexMetrica setLocation:location];
+        AMAAppMetrica.customLocation = location;
     }];
 }
 
@@ -79,42 +84,42 @@ static bool gYMMIsAppMetricaActivated = false;
 
     if (enabledValue != nil) {
         [self dispatchAsync:^{
-            [YMMYandexMetrica setLocationTracking:enabledValue.boolValue];
+            AMAAppMetrica.locationTrackingEnabled = enabledValue.boolValue;
         }];
     }
 }
 
-- (YMMECommerceScreen *)createScreen:(NSDictionary *)screen {
-    YMMECommerceScreen *screenObj = [[YMMECommerceScreen alloc] initWithName:screen[@"screenName"] categoryComponents:@[] searchQuery:screen[@"searchQuery"] payload:@{}];
+- (AMAECommerceScreen *)createScreen:(NSDictionary *)screen {
+    AMAECommerceScreen *screenObj = [[AMAECommerceScreen alloc] initWithName:screen[@"screenName"] categoryComponents:@[] searchQuery:screen[@"searchQuery"] payload:@{}];
     return screenObj;
 }
 
-- (YMMECommerceProduct *)createProduct:(NSDictionary *)product {
-    YMMECommerceAmount *actualFiat = [[YMMECommerceAmount alloc] initWithUnit:product[@"currency"] value:[NSDecimalNumber decimalNumberWithString:product[@"price"]]];
-   YMMECommercePrice *actualPrice = [[YMMECommercePrice alloc] initWithFiat:actualFiat internalComponents:@[]];
-    YMMECommerceProduct *productObj = [[YMMECommerceProduct alloc] initWithSKU:product[@"sku"] name:product[@"name"] categoryComponents:@[] payload:@{} actualPrice:actualPrice originalPrice:actualPrice promoCodes:@[]];
+- (AMAECommerceProduct *)createProduct:(NSDictionary *)product {
+    AMAECommerceAmount *actualFiat = [[AMAECommerceAmount alloc] initWithUnit:product[@"currency"] value:[NSDecimalNumber decimalNumberWithString:product[@"price"]]];
+    AMAECommercePrice *actualPrice = [[AMAECommercePrice alloc] initWithFiat:actualFiat internalComponents:@[]];
+    AMAECommerceProduct *productObj = [[AMAECommerceProduct alloc] initWithSKU:product[@"sku"] name:product[@"name"] categoryComponents:@[] payload:@{} actualPrice:actualPrice originalPrice:actualPrice promoCodes:@[]];
     return productObj;
 }
 
-- (YMMECommercePrice *)createPrice:(NSDictionary *)product {
-    YMMECommerceAmount *priceObj = [[YMMECommerceAmount alloc] initWithUnit:product[@"currency"] value:[NSDecimalNumber decimalNumberWithString:product[@"price"]]];
-    YMMECommercePrice *actualPrice = [[YMMECommercePrice alloc] initWithFiat:priceObj internalComponents:@[]];
+- (AMAECommercePrice *)createPrice:(NSDictionary *)product {
+    AMAECommerceAmount *priceObj = [[AMAECommerceAmount alloc] initWithUnit:product[@"currency"] value:[NSDecimalNumber decimalNumberWithString:product[@"price"]]];
+    AMAECommercePrice *actualPrice = [[AMAECommercePrice alloc] initWithFiat:priceObj internalComponents:@[]];
 
     return actualPrice;
 }
 
-- (YMMECommerceCartItem *)createCartItem:(NSDictionary *)product {
-    YMMECommerceScreen *screen = [self createScreen:@{}];
+- (AMAECommerceCartItem *)createCartItem:(NSDictionary *)product {
+    AMAECommerceScreen *screen = [self createScreen:@{}];
 
-    YMMECommerceProduct *productObj = [self createProduct:product];
+    AMAECommerceProduct *productObj = [self createProduct:product];
 
-     YMMECommerceReferrer *referrer = [[YMMECommerceReferrer alloc] initWithType:@"" identifier:@"" screen:screen];
+    AMAECommerceReferrer *referrer = [[AMAECommerceReferrer alloc] initWithType:@"" identifier:@"" screen:screen];
 
     NSDecimalNumber *quantity = [NSDecimalNumber decimalNumberWithString:product[@"quantity"]];
 
-    YMMECommercePrice *actualPrice = [self createPrice:product];
+    AMAECommercePrice *actualPrice = [self createPrice:product];
 
-    YMMECommerceCartItem *cartItem = [[YMMECommerceCartItem alloc]  initWithProduct:productObj quantity:quantity revenue:actualPrice referrer:referrer];
+    AMAECommerceCartItem *cartItem = [[AMAECommerceCartItem alloc]  initWithProduct:productObj quantity:quantity revenue:actualPrice referrer:referrer];
 
     return cartItem;
 }
@@ -123,8 +128,8 @@ static bool gYMMIsAppMetricaActivated = false;
 
 - (void)showScreen:(CDVInvokedUrlCommand *)command {
     NSDictionary* args = [command.arguments objectAtIndex:0];
-    YMMECommerceScreen *screenObj = [self createScreen:args];
-    [YMMYandexMetrica reportECommerce:[YMMECommerce showScreenEventWithScreen:screenObj] onFailure:nil];
+    AMAECommerceScreen *screenObj = [self createScreen:args];
+    [AMAAppMetrica reportECommerce:[AMAECommerce showScreenEventWithScreen:screenObj] onFailure:nil];
     CDVPluginResult* pluginResult = nil;
     if (args != nil) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ecommerce sending"];
@@ -137,9 +142,9 @@ static bool gYMMIsAppMetricaActivated = false;
 
 - (void)showProductCard:(CDVInvokedUrlCommand *)command {
     NSDictionary* args = [command.arguments objectAtIndex:0];
-    YMMECommerceScreen *screen = [self createScreen:@{}];
-    YMMECommerceProduct *productObj = [self createProduct:args];
-    [YMMYandexMetrica reportECommerce:[YMMECommerce showProductCardEventWithProduct:productObj screen:screen] onFailure:nil];
+    AMAECommerceScreen *screen = [self createScreen:@{}];
+    AMAECommerceProduct *productObj = [self createProduct:args];
+    [AMAAppMetrica reportECommerce:[AMAECommerce showProductCardEventWithProduct:productObj screen:screen] onFailure:nil];
     CDVPluginResult* pluginResult = nil;
     if (args != nil) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ecommerce sending"];
@@ -151,8 +156,8 @@ static bool gYMMIsAppMetricaActivated = false;
 
 - (void)addToCart:(CDVInvokedUrlCommand *)command {
     NSDictionary* args = [command.arguments objectAtIndex:0];
-    YMMECommerceCartItem *cartItem = [self createCartItem:args];
-    [YMMYandexMetrica reportECommerce:[YMMECommerce addCartItemEventWithItem:cartItem] onFailure:nil];
+    AMAECommerceCartItem *cartItem = [self createCartItem:args];
+    [AMAAppMetrica reportECommerce:[AMAECommerce addCartItemEventWithItem:cartItem] onFailure:nil];
     CDVPluginResult* pluginResult = nil;
     if (args != nil) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ecommerce sending"];
@@ -164,8 +169,8 @@ static bool gYMMIsAppMetricaActivated = false;
 
 - (void)removeFromCart:(CDVInvokedUrlCommand *)command {
     NSDictionary* args = [command.arguments objectAtIndex:0];
-    YMMECommerceCartItem *cartItem = [self createCartItem:args];
-    [YMMYandexMetrica reportECommerce:[YMMECommerce removeCartItemEventWithItem:cartItem] onFailure:nil];
+    AMAECommerceCartItem *cartItem = [self createCartItem:args];
+    [AMAAppMetrica reportECommerce:[AMAECommerce removeCartItemEventWithItem:cartItem] onFailure:nil];
     CDVPluginResult* pluginResult = nil;
     if (args != nil) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ecommerce sending"];
@@ -183,11 +188,11 @@ static bool gYMMIsAppMetricaActivated = false;
         for(int i=0; i< products.count; i++){
            [cartItems addObject:[self createCartItem:products[i]]];
         }
-        YMMECommerceOrder *order = [[YMMECommerceOrder alloc] initWithIdentifier:identifier
+    AMAECommerceOrder *order = [[AMAECommerceOrder alloc] initWithIdentifier:identifier
                                                                        cartItems:cartItems
                                                                          payload:@{}];
 
-        [YMMYandexMetrica reportECommerce:[YMMECommerce purchaseEventWithOrder:order] onFailure:nil];
+        [AMAAppMetrica reportECommerce:[AMAECommerce purchaseEventWithOrder:order] onFailure:nil];
     CDVPluginResult* pluginResult = nil;
     if (args != nil) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Ecommerce sending"];
@@ -210,13 +215,13 @@ static bool gYMMIsAppMetricaActivated = false;
     };
 }
 
-+ (YMMYandexMetricaConfiguration *)configurationForDictionary:(NSDictionary *)configurationDictionary
++ (AMAAppMetricaConfiguration *)configurationForDictionary:(NSDictionary *)configurationDictionary
 {
     NSString *apiKey = configurationDictionary[@"apiKey"];
-    YMMYandexMetricaConfiguration *configuration = [[YMMYandexMetricaConfiguration alloc] initWithApiKey:apiKey];
+
+    AMAAppMetricaConfiguration *configuration = [[AMAAppMetricaConfiguration alloc] initWithAPIKey:apiKey];
 
     NSNumber *handleFirstActivationAsUpdate = configurationDictionary[@"handleFirstActivationAsUpdate"];
-    NSNumber *locationTracking = configurationDictionary[@"locationTracking"];
     NSNumber *sessionTimeout = configurationDictionary[@"sessionTimeout"];
     NSNumber *crashReporting = configurationDictionary[@"crashReporting"];
     NSString *appVersion = configurationDictionary[@"appVersion"];
@@ -227,28 +232,17 @@ static bool gYMMIsAppMetricaActivated = false;
     if (handleFirstActivationAsUpdate != nil) {
         configuration.handleFirstActivationAsUpdate = [handleFirstActivationAsUpdate boolValue];
     }
-    if (locationTracking != nil) {
-        configuration.locationTracking = [locationTracking boolValue];
-    }
     if (sessionTimeout != nil) {
         configuration.sessionTimeout = [sessionTimeout unsignedIntegerValue];
-    }
-    if (crashReporting != nil) {
-        configuration.crashReporting = [crashReporting boolValue];
     }
     if (appVersion != nil) {
         configuration.appVersion = appVersion;
     }
-    if (logs != nil) {
-        configuration.logs = [logs boolValue];
-    }
-    if (customLocationDictionary != nil) {
-        configuration.location = [self locationForDictionary:customLocationDictionary];
-    }
+
     if (preloadInfoDictionary != nil) {
         NSString *trackingID = preloadInfoDictionary[@"trackingId"];
-        YMMYandexMetricaPreloadInfo *preloadInfo =
-            [[YMMYandexMetricaPreloadInfo alloc] initWithTrackingIdentifier:trackingID];
+        AMAAppMetricaPreloadInfo *preloadInfo =
+            [[AMAAppMetricaPreloadInfo alloc] initWithTrackingIdentifier:trackingID];
         NSDictionary *additionalInfo = preloadInfoDictionary[@"additionalParams"];
         for (NSString *key in additionalInfo) {
             [preloadInfo setAdditionalInfo:additionalInfo[key] forKey:key];
@@ -293,8 +287,8 @@ static bool gYMMIsAppMetricaActivated = false;
 
 + (void)activateWithConfigurationDictionary:(NSDictionary *)configuration
 {
-    YMMYandexMetricaConfiguration *config = [[self class] configurationForDictionary:configuration];
-    [YMMYandexMetrica activateWithConfiguration:config];
+    AMAAppMetricaConfiguration *config = [[self class] configurationForDictionary:configuration];
+    [AMAAppMetrica activateWithConfiguration:config];
     gYMMIsAppMetricaActivated = true;
 }
 
